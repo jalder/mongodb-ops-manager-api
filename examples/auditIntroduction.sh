1440091cff77d61d92e4dc569b750562779a5a3a
Jack Alder <jack.alder@mongodb.com>
2019-05-08 14:12:50 -0400

    moving to top vars

--- examples/auditIntroduction.sh
+++ examples/auditIntroduction.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-curl -u "{USERNAME}:{APIKEY}" --digest "http://{HOST:PORT}/api/public/v1.0/groups/{PROJECTID}/automationConfig" > /tmp/automationConfig.json
+USER="{USERNAME}"
+APIKEY="{APIKEY}"
+HOST="{HOST:PORT}"
+PROJECTID="{PROJECTID}"
+TMPDIR="/tmp"
 
-./jq -c --arg FILTER '{ atype: { $nin: [ "createCollection", "createDatabase" ] } }' '.processes |= map((select(.processType == "mongod") | .args2_6 += {"auditLog":{"destination":"file","format":"JSON","filter":$FILTER,"path":"/tmp/auditLog.json"}}) // .)' /tmp/automationConfig.json > /tmp/automationConfig-new.json
+curl -u "$USER:$APIKEY" --digest "http://$HOST/api/public/v1.0/groups/$PROJECTID/automationConfig" > $TMPDIR/automationConfig.json
 
-curl -u "{USERNAME}:{APIKEY}" -H "Content-Type: application/json" --digest -i -X PUT "http://{HOST:PORT}/api/public/v1.0/groups/{PROJECTID}/automationConfig" --data @/tmp/automationConfig-new.json
+./jq -c --arg FILTER '{ atype: { $nin: [ "createCollection", "createDatabase" ] } }' '.processes |= map((select(.processType == "mongod") | .args2_6 += {"auditLog":{"destination":"file","format":"JSON","filter":$FILTER,"path":"/tmp/auditLog.json"}}) // .)' /tmp/automationConfig.json > $TMPDIR/automationConfig-new.json
 
+curl -u "$USER:$APIKEY" -H "Content-Type: application/json" --digest -i -X PUT "http://$HOST/api/public/v1.0/groups/$PROJECTID/automationConfig" --data @$TMPDIR/automationConfig-new.json
